ec68341120f3c542af4f6803a0c542be3883267a
Szymon Nowak <szimek@gmail.com>
2010-02-21 20:36:39 +0100

    Removed FilterParamMatcher - filtered parameters are now set in application configuration.

--- lib/shoulda/action_controller/matchers.rb
+++ lib/shoulda/action_controller/matchers.rb
@@ -1,5 +1,4 @@
 require 'shoulda/action_controller/matchers/assign_to_matcher'
-require 'shoulda/action_controller/matchers/filter_param_matcher'
 require 'shoulda/action_controller/matchers/set_the_flash_matcher'
 require 'shoulda/action_controller/matchers/render_with_layout_matcher'
 require 'shoulda/action_controller/matchers/respond_with_matcher'
@@ -12,24 +11,24 @@ module Shoulda # :nodoc:
 
     # By using the macro helpers you can quickly and easily create concise and
     # easy to read test suites.
-    # 
+    #
     # This code segment:
-    # 
+    #
     #   describe UsersController, "on GET to show with a valid id" do
     #     before(:each) do
     #       get :show, :id => User.first.to_param
     #     end
-    # 
+    #
     #     it { should assign_to(:user) }
     #     it { should respond_with(:success) }
     #     it { should render_template(:show) }
     #     it { should not_set_the_flash) }
-    # 
+    #
     #     it "should do something else really cool" do
     #       assigns[:user].id.should == 1
     #     end
     #   end
-    # 
+    #
     # Would produce 5 tests for the show action
     module Matchers
     end
